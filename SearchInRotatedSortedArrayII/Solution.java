97142d52b515695de4f5c2cbef0be0878294bd63
Z.Huang <tercelhuang@gmail.com>
2016-01-30 18:52:30 -0800

    Binary search not applicble due to existence of duplicates

---
+++ SearchInRotatedSortedArrayII/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    /**
+     * param A : an integer ratated sorted array and duplicates are allowed
+     * param target :  an integer to be search
+     * return : a boolean
+     */
+    public boolean search(int[] A, int target) {
+        for (int i = 0; i < A.length; i ++) {
+            if (A[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
