088965f2eee56129d591071269fd218e56366dc9
Z.Huang <tercelhuang@gmail.com>
2016-02-02 01:06:12 -0800

    Recursion, binary tree, preorder traversal

---
+++ BinaryTreePreorderTraversal/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        // write your code here
+        ArrayList<Integer> preoder = new ArrayList<Integer>();
+
+        if (root == null) {
+            return preoder;
+        }
+
+        preoder.add(root.val);
+        preoder.addAll(preorderTraversal(root.left));
+        preoder.addAll(preorderTraversal(root.right));
+
+        return preoder;
+    }
+}
