6ac3e89310dbe7e5c611c6c584aefaf4796b9a48
Z.Huang <tercelhuang@gmail.com>
2016-02-03 00:50:27 -0800

    Add non-recursion version of preorder traversal, binary tree

--- BinaryTreePreorderTraversal/Solution.java
+++ BinaryTreePreorderTraversal/Solution.java
@@ -9,6 +9,7 @@
  *     }
  * }
  */
+// Version 2: Divide & Conquer
 public class Solution {
     /**
      * @param root: The root of binary tree.
@@ -29,3 +30,31 @@ public class Solution {
         return preoder;
     }
 }
+
+
+// Version 0: Non-Recursion (Recommend)
+
+// public class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         Stack<TreeNode> stack = new Stack<TreeNode>();
+//         List<Integer> preorder = new ArrayList<Integer>();
+//
+//         if (root == null) {
+//             return preorder;
+//         }
+//
+//         stack.push(root);
+//         while (!stack.empty()) {
+//             TreeNode node = stack.pop();
+//             preorder.add(node.val);
+//             if (node.right != null) {
+//                 stack.push(node.right);
+//             }
+//             if (node.left != null) {
+//                 stack.push(node.left);
+//             }
+//         }
+//
+//         return preorder;
+//     }
+// }
