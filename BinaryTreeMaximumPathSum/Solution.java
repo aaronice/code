2991725a8ce87e509b5390880565563005710cd1
Z.Huang <tercelhuang@gmail.com>
2016-02-04 01:39:42 -0800

    Divide and conquer, binary tree maximum path

--- BinaryTreeMaximumPathSum/Solution.java
+++ BinaryTreeMaximumPathSum/Solution.java
@@ -14,7 +14,43 @@ public class Solution {
      * @param root: The root of binary tree.
      * @return: An integer.
      */
+    private class ResultType {
+        // singlePath: the maximum path sum from root down to any node,
+        // the path can contain no node
+        // maxPath: the maximum path from any node to any node,
+        // the path contains at least one node
+        int singlePath, maxPath;
+
+        ResultType(int singlePath, int maxPath) {
+            this.singlePath = singlePath;
+            this.maxPath = maxPath;
+        }
+    }
+
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(0, Integer.MIN_VALUE);
+        }
+
+        // Divide
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+
+        // Conquer
+        int singlePath = Math.max(left.singlePath, right.singlePath) + root.val;
+        singlePath = Math.max(singlePath, 0);
+
+        int maxPath = Math.max(left.maxPath, right.maxPath);
+        maxPath = Math.max(maxPath,
+            left.singlePath + right.singlePath + root.val);
+
+        return new ResultType(singlePath, maxPath);
+    }
+
+
     public int maxPathSum(TreeNode root) {
         // write your code here
+        ResultType result = helper(root);
+        return result.maxPath;
     }
 }
