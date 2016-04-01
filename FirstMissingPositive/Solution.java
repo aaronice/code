33b152690afe2c5aaaddc73a67255c1332c34650
Z.Huang <tercelhuang@gmail.com>
2016-04-01 00:25:05 -0700

    Modified algorithm, array operation

--- FirstMissingPositive/Solution.java
+++ FirstMissingPositive/Solution.java
@@ -32,4 +32,29 @@ public class Solution {
         A[l] = A[r];
         A[r] = tmp;
     }
+
+    // SOLUTION 2:
+    public int firstMissingPositive2(int[] A) {
+        // bug 3: when length is 0, return 1;
+        if (A == null) {
+            return 0;
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            // 1: A[i] is in the range;
+            // 2: A[i] > 0.
+            // 3: The target is different;
+            while (A[i] <= A.length && A[i] > 0 && A[A[i] - 1] != A[i]) {
+                swap(A, i, A[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return A.length + 1;
+    }
 }
