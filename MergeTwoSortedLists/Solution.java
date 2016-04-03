eca56d3a4966c51de5238be8142c94e5c0267176
Z.Huang <tercelhuang@gmail.com>
2016-04-03 16:38:14 -0700

    linkedlist, dummy node, merge

---
+++ MergeTwoSortedLists/Solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param ListNode l1 is the head of the linked list
+     * @param ListNode l2 is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // write your code here
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = dummyHead;
+
+        while(l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+
+        if (l1 != null) {
+            p.next = l1;
+        }
+        if (l2 != null) {
+            p.next = l2;
+        }
+
+        return dummyHead.next;
+
+    }
+}
