4012165daab59106c1a95e4d81e850ba07a08129
Z.Huang <tercelhuang@gmail.com>
2016-04-07 01:18:57 -0700

    remove listnode, dummy head

---
+++ RemoveLinkedListElements/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     * @param head a ListNode
+     * @param val an integer
+     * @return a ListNode
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        // Write your code here
+        ListNode p = new ListNode(0);
+        p.next = head;
+        ListNode h = p;
+
+        while (p != null && p.next != null) {
+            if (p.next.val == val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return h.next;
+    }
+}
