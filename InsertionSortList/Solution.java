576c77918b094e8d31af9739a77f6793405aee97
Z.Huang <tercelhuang@gmail.com>
2016-04-10 18:13:30 -0700

    insertion sort; dummy head

---
+++ InsertionSortList/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    public ListNode insertionSortList(ListNode head) {
+        // Create a new dummy head to insert node to 
+        ListNode dummy = new ListNode(0);
+
+        while (head != null) {
+            ListNode node = dummy;
+            while (node.next != null && node.next.val < head.val) {
+                node = node.next;
+            }
+            ListNode temp = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = temp;
+        }
+        return dummy.next;
+    }
+}
