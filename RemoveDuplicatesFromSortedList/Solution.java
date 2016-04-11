93fc01c7ce9b6dfe01f9bfce9fa65958bf0290a2
Z.Huang <tercelhuang@gmail.com>
2016-04-10 17:43:47 -0700

    remove duplicates from sorted list

---
+++ RemoveDuplicatesFromSortedList/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param ListNode head is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+
+        ListNode nodeHead = head;
+        while (head != null && head.next != null) {
+            if (head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+
+        }
+        return nodeHead;
+    }
+}
