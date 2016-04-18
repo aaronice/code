d0c14cf8285622088f53495e2119ef7448f82ed5
Z.Huang <tercelhuang@gmail.com>
2016-04-17 17:02:35 -0700

    two pointers approach

---
+++ LinkedListCycle/Solution.java
@@ -0,0 +1,38 @@
+// Ref: https://leetcode.com/articles/linked-list-cycle/
+// Approach: Two Pointers
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {
+        // write your code here
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
