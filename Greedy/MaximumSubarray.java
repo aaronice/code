3c1f85421a494a4cc2ef53999e7e270dbce70765
Z.Huang <tercelhuang@gmail.com>
2016-04-24 17:22:39 -0700

    prefix sum dynamic programming, max subarray

--- Greedy/MaximumSubarray.java
+++ Greedy/MaximumSubarray.java
@@ -1,6 +1,15 @@
+/*
+Given an array of integers, find a contiguous subarray which has the largest sum.
+
+Example
+
+Given the array `[−2,2,−3,4,−1,2,1,−5,3]`, the contiguous subarray `[4,−1,2,1]` has the largest sum = `6`.
+*/
+
 public class Solution {
+    // Version 1: Greedy
     /**
-     * @param nums: A list of integers
+     * @param nums: An array of integers
      * @return: A integer indicate the sum of max subarray
      */
     public int maxSubArray(int[] nums) {
@@ -15,6 +24,23 @@ public class Solution {
             max = Math.max(sum, max);
             sum = Math.max(sum, 0);
         }
+        return max;
+    }
+
+
+    // Version 2: Prefix Sum (DP)
+    public int maxSubArrayDP(int[] A) {
+        if (A == null || A.length == 0){
+            return 0;
+        }
+
+        int max = Integer.MIN_VALUE, sum = 0, minSum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            max = Math.max(max, sum - minSum);
+            minSum = Math.min(minSum, sum);
+        }
+
         return max;
     }
 }
