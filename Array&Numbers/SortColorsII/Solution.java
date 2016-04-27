bc5502b278102eb0b39f5a58fb566e25dfb55614
Z.Huang <tercelhuang@gmail.com>
2016-04-27 00:42:05 -0700

    use two pointers, sort the array into three parts, sort colors ii

--- Array&Numbers/SortColorsII/Solution.java
+++ Array&Numbers/SortColorsII/Solution.java
@@ -7,8 +7,13 @@ with the colors in the order 1, 2, ... k.
  */
 
 public class Solution {
-    // O(k) space, O(n) time; two-pass algorithm, counting sort
-    public void sortColors2(int[] colors, int k) {
+    /**
+     * Method I: O(k) space, O(n) time; two-pass algorithm, counting sort
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */
+    public void sortColors2TwoPass(int[] colors, int k) {
         int[] count = new int[k];
         for (int color : colors) {
             count[color-1]++;
@@ -20,5 +25,60 @@ public class Solution {
                 count[i]--;
             }
         }
+    }
+
+    /**
+     * Method II:
+     *  Each time sort the array into three parts:
+     *  [all min] [all unsorted others] [all max],
+     *  then update min and max and sort the [all unsorted others]
+     *  with the same method.
+     *
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */
+    public void sortColors2(int[] colors, int k) {
+        int pl = 0;
+        int pr = colors.length - 1;
+        int i = 0;
+        int min = 1, max = k;
+        while (min < max) {
+            while (i <= pr) {
+                if (colors[i] == min) {
+                    swap(colors, pl, i);
+                    i++;
+                    pl++;
+                } else if (colors[i] == max) {
+                    swap(colors, pr, i);
+                    pr--;
+                } else {
+                    i++;
+                }
+                // printArray(colors);
+            }
+            i = pl;
+            min++;
+            max--;
+        }
+    }
+
+    private void swap(int[] colors, int i, int j) {
+        int temp = colors[i];
+        colors[i] = colors[j];
+        colors[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] colors = new int[]{2, 5, 3, 4, 2, 2, 1};
+        int k = 5;
+        s.sortColors2(colors, k);
+    }
+    private static void printArray(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.print("\n");
     }
 }
