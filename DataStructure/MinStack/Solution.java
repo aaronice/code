eed96186e8506fe8e948edcb331403522898e52a
Z.Huang <tercelhuang@gmail.com>
2016-06-12 16:31:13 -0700

    Min Stack, priority queue

---
+++ DataStructure/MinStack/Solution.java
@@ -0,0 +1,29 @@
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
+
+    public void push(int number) {
+        stack.push(number);
+        if (minStack.isEmpty()) {
+            minStack.push(number);
+        } else if (Integer.parseInt(minStack.peek().toString()) >= number) {
+            minStack.push(number);
+        }
+    }
+
+    public int pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        return stack.pop();
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
+}
